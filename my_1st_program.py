dab8bf0a0ea7a01ea73afcdb54300c6e33b3d1b7
Kopanye <Kopanyerams@gmail.com>
2025-10-22 15:05:05 +0200

    my program

---
+++ my_1st_program.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+surname = input("please enter your surname:")
+Summary = input("write a little bio about your self")
+
+print("Hi everyone I am " + name +" " + surname + " and here is a little about me I am a "+ Summary)
